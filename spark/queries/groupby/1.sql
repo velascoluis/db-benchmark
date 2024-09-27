4c566eec58b18c5aaa500676815e21adffa97bca
Luis Velasco <velascoluis@google.com>
2024-09-27 15:16:16 +0200

    Change q1

--- spark/queries/groupby/1.sql
+++ spark/queries/groupby/1.sql
@@ -1,2 +1,7 @@
-create temporary view ans as select id1, sum(v1) as v1 from x group by id1;
-select sum(v1) as v1 from ans;
+WITH ans AS (
+    SELECT id1, SUM(v1) AS v1
+    FROM G1_1e9_1e2_5_0
+    GROUP BY id1
+)
+SELECT SUM(v1) AS v1
+FROM ans;
