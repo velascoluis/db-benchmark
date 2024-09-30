c4001b5647529e43f6c905984e09e273543c1bb2
Luis Velasco <velascoluis@google.com>
2024-09-30 13:00:31 +0200

    Take out median query

--- spark/queries/groupby/6.sql
+++ spark/queries/groupby/6.sql
@@ -1,13 +1,13 @@
-WITH ans AS (
-    SELECT 
-        id4, 
-        id5, 
-        MEDIAN(v3) AS median_v3,  -- This function doesn't exist in Spark SQL
-        STDDEV(v3) AS sd_v3
-    FROM G1_1e9_1e2_5_0
-    GROUP BY id4, id5
-)
+--WITH ans AS (
+--    SELECT 
+ --       id4, 
+ --       id5, 
+--        MEDIAN(v3) AS median_v3,  -- This function doesn't exist in Spark SQL
+--        STDDEV(v3) AS sd_v3
+--    FROM G1_1e9_1e2_5_0
+--    GROUP BY id4, id5
+--)
 -- Check query
-SELECT SUM(median_v3) AS median_v3, SUM(sd_v3) AS sd_v3
-FROM ans;
+--SELECT SUM(median_v3) AS median_v3, SUM(sd_v3) AS sd_v3
+--FROM ans;
 
